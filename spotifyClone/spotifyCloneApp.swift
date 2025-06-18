5e19b61af7e81f6306d2a58548be520f7c5ccbbd
rsaniceto14 <raphaelaniceto14@gmail.com>
2025-06-18 11:31:40 -0300

    Initial Commit

---
+++ spotifyClone/spotifyCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  spotifyCloneApp.swift
+//  spotifyClone
+//
+//  Created by Raphael Aniceto on 18/06/25.
+//
+
+import SwiftUI
+
+@main
+struct spotifyCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
